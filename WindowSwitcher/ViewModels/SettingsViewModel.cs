d2183956bbb21e893052e0b0ad80d6f13adab316
Sébastien Duruz <sebastienduruz@protonmail.com>
2025-12-30 19:18:19 +0100

    feat: Use proper bindings for the settings

--- WindowSwitcher/ViewModels/SettingsViewModel.cs
+++ WindowSwitcher/ViewModels/SettingsViewModel.cs
@@ -1,10 +1,103 @@
+using System;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using WindowSwitcherLib.Data.FileAccess;
 using WindowSwitcherLib.Models;
 
 namespace WindowSwitcher.ViewModels;
 
-public class SettingsViewModel
+public class SettingsViewModel : ObservableObject
 {
-    public ConfigFile ConfigFile { get; } = ConfigFileAccessor.GetInstance().Config;
+    private readonly ConfigFile _configFile = ConfigFileAccessor.GetInstance().Config;
+
+    public SettingsViewModel(Action applyAction)
+    {
+        ApplyCommand = new RelayCommand(applyAction);
+    }
+
+    public IRelayCommand ApplyCommand { get; }
+
+    public bool StartMinimized
+    {
+        get => _configFile.StartMinimized;
+        set
+        {
+            if (_configFile.StartMinimized == value)
+                return;
+            _configFile.StartMinimized = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool ResizeWindows
+    {
+        get => _configFile.ResizeWindows;
+        set
+        {
+            if (_configFile.ResizeWindows == value)
+                return;
+            _configFile.ResizeWindows = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool MoveWindows
+    {
+        get => _configFile.MoveWindows;
+        set
+        {
+            if (_configFile.MoveWindows == value)
+                return;
+            _configFile.MoveWindows = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool ShowWindowDecorations
+    {
+        get => _configFile.ShowWindowDecorations;
+        set
+        {
+            if (_configFile.ShowWindowDecorations == value)
+                return;
+            _configFile.ShowWindowDecorations = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool UseFixedWindowSize
+    {
+        get => _configFile.UseFixedWindowSize;
+        set
+        {
+            if (_configFile.UseFixedWindowSize == value)
+                return;
+            _configFile.UseFixedWindowSize = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int WindowWidth
+    {
+        get => _configFile.WindowWidth;
+        set
+        {
+            if (_configFile.WindowWidth == value)
+                return;
+            _configFile.WindowWidth = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int WindowHeight
+    {
+        get => _configFile.WindowHeight;
+        set
+        {
+            if (_configFile.WindowHeight == value)
+                return;
+            _configFile.WindowHeight = value;
+            OnPropertyChanged();
+        }
+    }
 }
--- WindowSwitcher/Windows/SettingsWindow.axaml.cs
+++ WindowSwitcher/Windows/SettingsWindow.axaml.cs
@@ -1,9 +1,6 @@
-using System;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using WindowSwitcher.ViewModels;
 using WindowSwitcherLib.Data;
-using WindowSwitcherLib.Data.FileAccess;
 
 namespace WindowSwitcher.Windows;
 
@@ -12,7 +9,7 @@ public partial class SettingsWindow : Window
     public SettingsWindow()
     {
         InitializeComponent();
-        DataContext = new SettingsViewModel();
+        DataContext = new SettingsViewModel(MainWindow.ApplySettings);
         Closing += OnClosing;
     }
     
@@ -22,53 +19,4 @@ public partial class SettingsWindow : Window
         Hide();
     }
 
-    private void StartMinimizedCheckedChange(object? sender, RoutedEventArgs e)
-    {
-        ConfigFileAccessor.GetInstance().Config.StartMinimized =
-            ((CheckBox)sender!).IsChecked!.Value;
-    }
-    
-    private void ResizeWindowsCheckedChange(object? sender, RoutedEventArgs e)
-    {
-        ConfigFileAccessor.GetInstance().Config.ResizeWindows =
-            ((CheckBox)sender!).IsChecked!.Value;
-    }
-    
-    private void MoveWindowsCheckedChange(object? sender, RoutedEventArgs e)
-    {
-        ConfigFileAccessor.GetInstance().Config.MoveWindows =
-            ((CheckBox)sender!).IsChecked!.Value;
-    }
-
-    private void ShowWindowDecorationsCheckedChange(object? sender, RoutedEventArgs e)
-    {
-        ConfigFileAccessor.GetInstance().Config.ShowWindowDecorations =
-            ((CheckBox)sender!).IsChecked!.Value;
-    }
-    
-    private void ApplyButtonClick(object? sender, RoutedEventArgs e)
-    {
-        MainWindow.ApplySettings();
-    }
-
-    private void FixedWindowCheckedChange(object? sender, RoutedEventArgs e)
-    {
-        ConfigFileAccessor.GetInstance().Config.UseFixedWindowSize =
-            ((CheckBox)sender!).IsChecked!.Value;
-
-        FixedWidthStackPanel.IsVisible = ((CheckBox)sender!).IsChecked!.Value;
-        FixedHeightStackPanel.IsVisible = ((CheckBox)sender!).IsChecked!.Value;
-    }
-
-    private void FixedWidthNumericChange(object? sender, NumericUpDownValueChangedEventArgs e)
-    {
-        ConfigFileAccessor.GetInstance().Config.WindowWidth =
-            (int)((NumericUpDown)sender!).Value.Value;
-    }
-    
-    private void FixedHeightNumericChange(object? sender, NumericUpDownValueChangedEventArgs e)
-    {
-        ConfigFileAccessor.GetInstance().Config.WindowHeight =
-            (int)((NumericUpDown)sender!).Value.Value;
-    }
 }
