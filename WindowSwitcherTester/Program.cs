8902ee80e00394f6bc7caf04530b54e467ed3a7d
Sébastien Duruz <sebastienduruz@protonmail.com>
2026-01-04 09:24:46 +0100

    chore: minor update to test project

--- WindowSwitcherTester/Program.cs
+++ WindowSwitcherTester/Program.cs
@@ -1,4 +1,8 @@
-﻿WindowAccessor accessor = WindowFactories.GetAccessor();
+﻿using System.Collections.ObjectModel;
+using WindowSwitcherLib.Data.WindowAccess;
+using WindowSwitcherLib.Models;
+
+WindowAccessor accessor = WindowFactories.GetAccessor();
 
 // Get the opened windows and raise the last one to front
 ObservableCollection<WindowConfig> windows = accessor.GetWindows();
